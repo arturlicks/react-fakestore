76ed68c5a6be5422e76de6f6cd7a75acfbee8b2a
Artur Licks <arturlicks@gmail.com>
2026-07-03 16:01:23 -0400

    Created the loading page for the cards while the API is responding.

---
+++ app/products/[id]/loading.tsx
@@ -0,0 +1,31 @@
+const CardLoading = () => {
+  return (
+    <div className="w-full justify-items-center">
+      <div className="flex flex-col shadow-xl rounded-2xl p-4 w-xl animate-pulse">
+        
+        {/* imagem */}
+        <div className="bg-gray-300 h-64 w-full rounded-lg"></div>
+
+        {/* título */}
+        <div className="bg-gray-300 h-6 w-3/4 mt-4 rounded"></div>
+
+        {/* categoria */}
+        <div className="bg-gray-300 h-4 w-1/2 mt-2 rounded"></div>
+
+        {/* preço */}
+        <div className="bg-gray-300 h-5 w-1/3 mt-2 rounded"></div>
+
+        {/* descrição */}
+        <div className="bg-gray-300 h-4 w-full mt-4 rounded"></div>
+        <div className="bg-gray-300 h-4 w-full mt-2 rounded"></div>
+        <div className="bg-gray-300 h-4 w-5/6 mt-2 rounded"></div>
+
+        {/* botões */}
+        <div className="bg-gray-300 h-10 w-full mt-4 rounded"></div>
+        <div className="bg-gray-300 h-10 w-full mt-2 rounded"></div>
+      </div>
+    </div>
+  );
+};
+
+export default CardLoading;
